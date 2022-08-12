3d0146f8c0edbc0de287567ea5704faa5bf0ac46
malithiperera <malithiperera1998@gmail.com>
2022-08-12 09:23:37 +0530

    url updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 const config = {
     // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
     // like '/berry-material-react/react/default'
-    basename: '/free',
+    basename: '/workzone',
     defaultPath: '/dashboard/default',
-    fontFamily: `'Roboto', sans-serif`,
+    fontFamily: `'poppins', sans-serif`,
     borderRadius: 12
 };
 
