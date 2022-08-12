2a427f05ea225c1e40166dde7b40f65acc16e5d1
malithiperera <malithiperera1998@gmail.com>
2022-08-12 22:52:03 +0530

    structure leave

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,17 +1,40 @@
+import { Typography } from '@mui/material';
+import { padding } from '@mui/system';
 const CardStyle = {
-    width: '300px',
+    width: '250px',
     backgroundColor: 'white',
-    height: '300px'
+    height: '120px',
+    borderRadius: '10px',
+    padding: '20px'
+    // display: 'flex',
+    // flexDirection: 'coloumn'
 };
-const mainContent = {};
+const mainContent = {
+    textAlign: 'center',
+    fontSize: '23px',
+    paddingTop: '5px'
+};
 
-const subContent = {};
+const subContent = {
+    textAlign: 'center',
+    fontSize: '28px',
+    padding: '5px',
+    color: '#1E4084'
+};
 const Card = () => {
     return (
         <>
             <div style={CardStyle}>
-                {/* <div style={mainContent}></div>
-                <div style={subContent}></div> */}
+                <div style={mainContent}>
+                    <Typography variant="h3" component="h2" style={mainContent}>
+                        No. of Leaves
+                    </Typography>
+                </div>
+                <div style={subContent}>
+                    <Typography variant="h3" component="h2" style={subContent}>
+                        10
+                    </Typography>
+                </div>
             </div>
         </>
     );
