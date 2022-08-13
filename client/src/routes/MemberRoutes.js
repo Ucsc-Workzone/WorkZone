a5257bf8fdcb64122ed310af1b7e893644463527
malithiperera <malithiperera1998@gmail.com>
2022-08-13 16:54:59 +0530

    update member routes

--- client/src/routes/MemberRoutes.js
+++ client/src/routes/MemberRoutes.js
@@ -7,13 +7,6 @@ import Loadable from 'ui-component/Loadable';
 // dashboard routing
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
-// utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
-const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
-
 // sample page routing
 const SamplePage = Loadable(lazy(() => import('views/Home')));
 
@@ -24,16 +17,12 @@ const MemberLeave = Loadable(lazy(() => import('views/Member/Todo')));
 const MemberProfile = Loadable(lazy(() => import('views/Member/Profile')));
 const MemberProjects = Loadable(lazy(() => import('views/Member/Profile')));
 
-const Todo = Loadable(lazy(() => import('views/Member/Leave')));
+const Todo = Loadable(lazy(() => import('views/Member/Todo')));
 
 const MemberRoutes = {
     path: '/',
     element: <MainLayout />,
     children: [
-        {
-            path: '/',
-            element: <SamplePage />
-        },
         {
             path: 'dashboard',
             children: [
@@ -83,7 +72,7 @@ const MemberRoutes = {
             path: 'member',
             children: [
                 {
-                    path: 'todo ',
+                    path: 'todo',
                     element: <Todo />
                 }
             ]
