f10560c030a2907255328ae9164218bb2a042a1e
= <pamodhamahagamage98@gmail.com>
2022-08-19 09:05:13 +0530

    chart js components added and dashboard annoucements updated

--- client/src/Components/UpcomingsList.js
+++ client/src/Components/UpcomingsList.js
@@ -1,13 +1,14 @@
 
+import { useState } from "react";
+
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { Pie, defaults } from 'react';
-import Chart from 'Components/chart';
-import { Doughnut } from 'react-chartjs-2';
+import DoughtChart from 'Components/DoughtChart';
 
 function createData(name, date) {
   return { name, date };
 }
+
 
 const rows = [
   createData('Malithi Perera', '27/08/2022'),
@@ -15,41 +16,51 @@ const rows = [
   createData('Malithi Perera', '27/08/2022')
 ];
 
+const projects ={
+  id:1,
+  name:"Mobile App",
+  pending:"4 Days",
+  date:"27/03/2022"
+}
 import './styles/upcoming.css';
 import { Typography } from '@mui/material';
 
-defaults.global.tooltips.enabled = false
-defaults.global.legend.position = 'bottom'
-
 const UpcomingList = () => {
-    return ( 
 
+  const [userData, setUserData] = useState({
+    // labels: ["CM", "NC"],
+    datasets: [
+      {
+        label: "Users Gained",
+        data: [85, 15],
+        backgroundColor: [
+          "#0f65fa",
+          "#c2c6d1",
+        ],
+        borderColor: "white",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+    return ( 
     
         <div className="upcomings">
              <Box style={{display: 'flex', justifyContent: 'right'}}>
-                <Button variant="contained"><Typography className="seeall" >Other Projects</Typography></Button>
+                <Box ><Button variant="contained" ><Typography className="seeall" >Other Projects</Typography></Button></Box>
             </Box>
-            <Box>
-              <Box style={{display:"flex", justifyContent: 'left', width:'60%', paddingLeft:'10px'}}>
+            <Box style={{display:"flex", width:'100%'}}>
+              <Box style={{display:"flex", justifyContent: 'left', width:'60%', paddingLeft:'40px', paddingTop:"40px", paddingBottom:"20px"}}>
                 <stack >
-                <Typography variant="h3" component="p" fontSize="1.5vw" paddingTop={"15px"} paddingBottom={"15px"}>Mobile App</Typography>
-                <Typography variant="p" component="p" fontSize="1.0vw"paddingBottom={"6px"}>4 Days Remaining</Typography>
-                <Typography variant="h4" component="p"  fontSize="1.2vw" paddingBottom={"10px"}>Due Date:27/03/2022</Typography>
+                <Typography variant="h3" component="p" fontSize="2vw" paddingTop={"15px"} paddingBottom={"15px"}>{projects.name}</Typography>
+                <Typography variant="p" component="p" fontSize="1.3vw" paddingBottom={"6px"} colour="yellow">{projects.pending} Remaining</Typography>
+                <Typography variant="h4" component="p"  fontSize="1.5vw" paddingBottom={"15px"}>Due Date:{projects.date}</Typography>
                 </stack>
-
-                
               </Box>
-              <Box style={{display:"flex", justifyContent: 'right', width:'40%'}}>
-
-              <div>
-                <Doughnut data={{
-                  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
-              
-              }} />
-                  {/* 
-                   */}
-              </div>
-              
+              <Box style={{display:"flex", justifyContent: 'center', width:'60%', top: "50%"}}>
+                <div style={{ width: "45%"}}>
+                  <DoughtChart chartData={userData} percentage={"80"}/>
+                </div>             
               </Box>
             </Box>
 
