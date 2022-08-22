285b58da9696223a2fa3ac551b9791138c49d09d
malithiperera <malithiperera1998@gmail.com>
2022-08-22 11:47:50 +0530

    dadmin dashboard

--- client/src/views/Admin/Dashboard.js
+++ client/src/views/Admin/Dashboard.js
@@ -1,16 +1,36 @@
 import AllUser from 'Components/AllUsers';
 import Calender from 'Components/Calender';
 import RecentAnnoucements from 'Components/RecentAnnouncements';
+import EarningCard from 'ui-component/cards/Skeleton/EarningCard';
+import TotalOrderLineChartCard from 'views/dashboard/Default/TotalOrderLineChartCard';
 import './styles/Home.css';
 const Dashboard = () => {
     return (
         <>
             <div className="mainContainer">
                 <div className="leftConatiner">
-                    <div className="cardConatiner"></div>
-                    <div className="annouceContainer"></div>
+                    <div className="cardConatiner">
+                        <div className='subCard'>
+                        <TotalOrderLineChartCard
+                        title={'Registered User'}
+                        count={20}
+                        icon={''}
+                        />
+                        </div>
+                        <div className='subCard'>
+                        <TotalOrderLineChartCard
+                        title={'Registered User '}
+                        count={10}
+                        icon={''}
+                        />
+                        </div>
+
+                    </div>
+                    <div className="annouceContainer">
+                    <RecentAnnoucements />
+                    </div>
                     <div className="feedBack">
-                        <RecentAnnoucements />
+                        
                     </div>
                 </div>
                 <div className="rightContainer">
