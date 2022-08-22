e6af35251d5baf1ce0b2b59ee1cd42642ca2e9f7
WeerasingheDPS <2019cs186@stu.ucsc.lk>
2022-08-22 13:38:30 +0530

    add admin routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import themes from 'themes';
 
 // project imports
 import NavigationScroll from 'layout/NavigationScroll';
-//
+
 // ==============================|| APP ||============================== //
 
 const App = () => {
