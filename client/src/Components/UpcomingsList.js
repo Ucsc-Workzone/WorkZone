bd7d22704ebca2c19429e4617b59d90105cc7894
= <pamodhamahagamage98@gmail.com>
2022-08-22 06:32:00 +0530

    Upcoming project card Fixed

--- client/src/Components/UpcomingsList.js
+++ client/src/Components/UpcomingsList.js
@@ -52,25 +52,28 @@ const UpcomingList = () => {
              <Box style={{display: 'flex', justifyContent: 'right'}}>
                 <Box ><Button variant="contained" ><Typography className="seeall" >Other Projects</Typography></Button></Box>
             </Box>
-            <Card variant="outlined">
+
+            <Box className="tag-container">
+            <Card variant="outlined" className="project-card">
             <Box style={{display:"flex", width:'100%'}}>
               <Box style={{display:"flex", justifyContent: 'left', width:'5%',backgroundColor:'#5e35b1'}}>
                 
               </Box>
-              <Box style={{display:"flex", justifyContent: 'left', width:'60%', paddingLeft:'40px'}}>
+              <Box style={{display:"flex", justifyContent: 'left', width:'70%', paddingLeft:'70px'}}>
                 <Stack >
-                <Typography variant="h3" component="p" fontSize="2vw" paddingTop={"15px"} paddingBottom={"15px"}>{projects.name}</Typography>
-                <Typography variant="p" component="p" fontSize="1.2vw" paddingBottom={"6px"}>{projects.pending} Remaining</Typography>
-                <Typography variant="h4" component="p"  fontSize="1.5vw" paddingBottom={"15px"}>Due Date:{projects.date}</Typography>
+                <Typography variant="h3" component="p" fontSize="1.5vw" paddingTop={"15px"} paddingBottom={"15px"}>{projects.name}</Typography>
+                <Typography variant="p" component="p" fontSize="0.8vw" paddingBottom={"6px"}>{projects.pending} Remaining</Typography>
+                <Typography variant="h4" component="p"  fontSize="1.0vw" paddingBottom={"15px"}>Due Date:{projects.date}</Typography>
                 </Stack>
               </Box>
-              <Box style={{display:"flex", justifyContent: 'center', width:'60%', top: "50%"}}>
+              <Box style={{display:"flex", justifyContent: 'left', width:'60%', top: "50%"}}>
                 <div style={{ width: "38%"}}>
                   <DoughtChart chartData={userData} percentage={"85"}/>
                 </div>             
               </Box>
             </Box>
-            </Card>
+            
+            </Card></Box>
 
         </div>
      );
