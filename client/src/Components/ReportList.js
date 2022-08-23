db1e73bb55e05968ad5a0b1413f533f34697d1d4
malithiperera <malithiperera1998@gmail.com>
2022-08-23 09:37:31 +0530

    work reports update

---
+++ client/src/Components/ReportList.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Button  from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+
+const Demo = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
+export default function InteractiveList() {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+        <Grid item xs={12} md={6}>
+         
+          <Demo>
+            <List dense={dense}>
+             
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Admin Prototype Complte"
+                    secondary='Sub Task 001'
+                  />
+                </ListItem>
+              
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Member Prototype Complte"
+                    secondary='Sub Task 001 Skelton web app'
+                  />
+                </ListItem>
+              
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="React application Initialize"
+                    secondary='Sub Task 001 Skelton web app'
+                  />
+                </ListItem>
+              
+            </List>
+            <Grid>
+            <Button variant="contained">View</Button>
+            <Button variant="contained">View</Button>
+            </Grid>
+          
+          </Demo>
+        </Grid>
+     
+    </Box>
+  );
+}
