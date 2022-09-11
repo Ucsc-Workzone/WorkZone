0726d8dc05a64979ef02636c87266036b9a595d3
= <pamodhamahagamage98@gmail.com>
2022-09-11 12:37:36 +0530

    orgregistratiom

--- client/src/views/SysAdmin/Feedback.js
+++ client/src/views/SysAdmin/Feedback.js
@@ -1,9 +1,17 @@
-const Feedbacks = () => {
-    return ( 
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { Box } from "@mui/system";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import Feedback from "Components/Feedback";
+const SysAdminFeedback = () => {
+    return (
         <>
-        <h2>Feed Backs</h2>
+            <Box style = {{width:"100%" , justifyContent:"space-between"}}><Feedback></Feedback></Box>
         </>
-     );
-}
- 
-export default Feedbacks;
+    );
+};
+
+export default SysAdminFeedback;
