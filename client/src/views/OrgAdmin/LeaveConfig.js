58717ac13200c9709cb82d793742bb00bc5ba233
= <pamodhamahagamage98@gmail.com>
2022-09-11 19:30:49 +0530

    leaveconfig route fixing and Datepicker constraints fixing

---
+++ client/src/views/OrgAdmin/LeaveConfig.js
@@ -0,0 +1,10 @@
+import RegisterUsers from '../pages/authentication/authentication3/RegisterUsers';
+const RegisterUser = () => {
+    return ( 
+        <>
+        <RegisterUsers />
+        </>
+     );
+}
+ 
+export default RegisterUser;
