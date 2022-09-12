005bbbb51cfa6923945f09333a1bc3dfb6a8a21e
malithiperera <malithiperera1998@gmail.com>
2022-09-12 16:56:00 +0530

    app todo

--- client/src/views/Coordinator/[projectid].js
+++ client/src/views/Coordinator/[projectid].js
@@ -1,14 +1,21 @@
+import App_todo_1 from '../../App_todo _1';
+import './styles/project_create.css';
+
 const ProjectPage = () => {
     return (
         <>
             <div className="projectmain">
-                <div>
-                    {/* <CBoard /> */}
+                <div className='todoContainer'>
+               <App_todo_1></App_todo_1>
                     <h2>This is jsjs</h2>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div className="memberAssign">
+                    <div className="projectHistory">
+
+                    </div>
+                    <div className="memberAssign">
+
+                    </div>
                 </div>
             </div>
         </>
