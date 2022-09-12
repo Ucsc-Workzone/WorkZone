9b51e644f29f0f8ae50449d1ce23092d4edbc6d3
KavindhuG <kavidhuhasith@gmail.com>
2022-09-12 14:04:03 +0530

    Edit Profile Section

--- client/src/Components/rightbar/Rightbar.jsx
+++ client/src/Components/rightbar/Rightbar.jsx
@@ -12,86 +12,44 @@ export default function Rightbar({ profile }) {
             <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
           </span>
         </div>
-        <img className="rightbarAd" src="assets/ad.png" alt="" />
-        <h4 className="rightbarTitle">Online Friends</h4>
-        <ul className="rightbarFriendList">
-          {Users.map((u) => (
+        {/* <img className="rightbarAd" src="assets/ad.png" alt="" /> */}
+        {/* <h4 className="rightbarTitle">Online Friends</h4> */}
+        {/* <ul className="rightbarFriendList"> */}
+          {/* {Users.map((u) => (
             <Online key={u.id} user={u} />
-          ))}
-        </ul>
+          ))} */}
+        {/* </ul> */}
       </>
     );
   };
 
   const ProfileRightbar = () => {
-    return (
-      <>
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">New York</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Madrid</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">Single</span>
-          </div>
+      return (
+          <>
+        <div className="mainContainer">
+        <div className="leftContainer">
+
+            <h4 className="rightbarTitle">User information</h4>
+            <div className="rightbarInfo">
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">City:</span>
+                <span className="rightbarInfoValue">Colombo</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">Age</span>
+                <span className="rightbarInfoValue">23</span>
+            </div>
+            <div className="rightbarInfoItem">
+                <span className="rightbarInfoKey">Joined Date:</span>
+                <span className="rightbarInfoValue">08.08.2022</span>
+            </div>
+            </div>
         </div>
-        <h4 className="rightbarTitle">User friends</h4>
-        <div className="rightbarFollowings">
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/1.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/2.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/3.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/4.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/5.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
-          <div className="rightbarFollowing">
-            <img
-              src="assets/person/6.jpeg"
-              alt=""
-              className="rightbarFollowingImg"
-            />
-            <span className="rightbarFollowingName">John Carter</span>
-          </div>
+            <div className="rightContainer">
+            <h4 className="rightbarTitle">Profile</h4>
+            </div>
         </div>
+        
       </>
     );
   };
--- client/src/views/Coordinator/Profile.js
+++ client/src/views/Coordinator/Profile.js
@@ -1,36 +1,33 @@
 import "./styles/Profile.css";
-import Topbar from "./../../Components/topbar/Topbar";
-import Sidebar from "./../../Components/sidebar/Sidebar";
-import Feed from "./../../Components/feed/Feed";
+import userimg from './../../assets/images/Profile/member2.jpg';
+import cvrimg from './../../assets/profile/1.jpeg'; 
 import Rightbar from "./../../Components/rightbar/Rightbar";
 const CoordinatorProfile = () => {
     return (
         <>
 <>
-      <Topbar />
+      
       <div className="profile">
-        <Sidebar />
         <div className="profileRight">
-          <div className="profileRightTop">
-            <div className="profileCover">
-              <img
-                className="profileCoverImg"
-                src="assets/post/3.jpeg"
-                alt=""
-              />
-              <img
-                className="profileUserImg"
-                src="assets/person/7.jpeg"
-                alt=""
-              />
-            </div>
-            <div className="profileInfo">
-                <h4 className="profileInfoName">Safak Kocaoglu</h4>
-                <span className="profileInfoDesc">Hello my friends!</span>
-            </div>
-          </div>
+     <div className="profileRightTop">
+                  <div className="profileCover">
+                    <img
+                      className="profileCoverImg"
+                      src={cvrimg}
+                      alt=""
+                    />
+                    <img
+                      className="profileUserImg"
+                      src={userimg}
+                      alt=""
+                    />
+                  </div>
+                  <div className="profileInfo">
+                      <h4 className="profileInfoName">Pamodha Mahagamage</h4>
+                      <span className="profileInfoDesc">Time is Every Thing</span>
+                  </div>
+                </div>
           <div className="profileRightBottom">
-            <Feed />
             <Rightbar profile/>
           </div>
         </div>
