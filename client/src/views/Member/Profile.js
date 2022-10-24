6e4981aea6848613c88a795eecd2ed48f39d4be7
Bimsara Kulasekara <bkdeveloping@gmail.com>
2022-10-24 19:44:00 +0530

    Profile Left

--- client/src/views/Member/Profile.js
+++ client/src/views/Member/Profile.js
@@ -1,302 +1,33 @@
+
+
+import React, { useState } from 'react';
 import './styles/profile.css';
-// import React,{useState} from 'react';
-// import TextField from '@mui/material/TextField';
-// import Button from '@mui/material/Button';
-// import '../../Components/styles/profile.css'
-// const ImgUpload =({
-//     onChange,
-//     src
-//   })=>
-//   <label htmlFor="photo-upload" className="upload">
-//   <div className="img-wrap img-upload" >
-//     <img for="photo-upload" src={src}/>
-//   </div>
-//   <input id="photo-upload" type="file" onChange={onChange}/> 
-// </label>
+import coverimg from '../../assets/images/cover.jpg';
 
 
-//   const Name =({
-//     onChange,
-//     value
-//   })=>
-//   <div className="field">
-//   <TextField
-//       id="standard-password-input"
-//       label="name"
-//       type="text"
-//       variant="standard"
-//       onChange={onChange} 
-//     maxlength="25" 
-//     value={value} 
-//     />
-
-// </div>
-
-
-//   const Age =({
-//     onChange,
-//     value
-//   })=>
-//     <div className="field">
-//       <TextField
-//           id="standard-password-input"
-//           label="age"
-//           type="int"
-//           variant="standard"
-//           onChange={onChange} 
-//         maxlength="25" 
-//         value={value} 
-//         />
-
-//     </div>
-
-// const Gender =({
-//     onChange,
-//     value
-//   })=>
-//   <div className="field">
-//   <TextField
-//       id="standard-password-input"
-//       label="gender"
-//       type="text"
-//       variant="standard"
-//       onChange={onChange} 
-//     maxlength="25" 
-//     value={value} 
-//     />
-
-// </div>
-
-// const Mobile =({
-//     onChange,
-//     value
-//   })=>
-//   <div className="field">
-//   <TextField
-//       id="standard-password-input"
-//       label="mobile"
-//       type="int"
-//       variant="standard"
-//       onChange={onChange} 
-//     maxlength="10" 
-//     value={value} 
-//     />
-
-// </div>
-
-// const Address =({
-//     onChange,
-//     value
-//   })=>
-//   <div className="field">
-//   <TextField
-//       id="standard-password-input"
-//       label="address"
-//       type="text"
-//       variant="standard"
-//       onChange={onChange} 
-//     maxlength="50" 
-//     value={value} 
-//     />
-
-// </div>
-
-// const Bio =({
-//     onChange,
-//     value
-//   })=>
-//   <div className="field">
-//   <TextField
-//       id="standard-password-input"
-//       label="bio"
-//       type="text"
-//       variant="standard"
-//       onChange={onChange} 
-//     maxlength="1000" 
-//     value={value} 
-//     />
-
-// </div>
-
-
-//   const Profile =({
-//     onSubmit,
-//     src,
-//     name,
-//     age,
-//     gender,
-//     mobile,
-//     address,
-//     bio,
-//   })=>
-
-
-
-//        <form className="form-m" onSubmit={onSubmit}>
-
-//       <label >
-//         <div className="img-wrap" >
-//           <img for="photo-upload" src={src}/>
-//         </div>
-//       </label>
-//       <div className="dis">
-//       <div className="name">Name : {name}
-
-//         </div>
-//         <div className="age">Age : {age}</div>
-//         <div className="gender">Gender : {gender}</div>
-//         <div className="mobile">Mobile : {mobile}</div>
-//         <div className="address">Address : {address}</div>
-//         <div className="bio">Bio : {bio}</div>
-//         </div>
-//       <Button  type="submit" className="save"  variant="contained" size="medium" >
-//           Edit profile
-//         </Button> 
-
-
-//       </form>
-
-
-
-//   const Edit =({
-//     onSubmit,
-//     children,
-//   })=>
-
-//       <form  onSubmit={onSubmit}>
-
-//           {children}
-//           <Button  type="submit" className="save"  variant="contained" size="medium" >
-//           Save
-//         </Button>
-
-//       </form>
-
-//   class CardProfile extends React.Component {
-//     state = {
-//       file: '',
-//       imagePreviewUrl: 'https://github.com/OlgaKoplik/CodePen/blob/master/profile.jpg?raw=true',
-//       name:'',
-//       age:'',
-//       gender:'',
-//       mobile:'',
-//       address:'',
-//       bio:'',
-//       active: 'edit'
-//     }
-
-//     photoUpload = e =>{
-//       e.preventDefault();
-//       const reader = new FileReader();
-//       const file = e.target.files[0];
-//       reader.onloadend = () => {
-//         this.setState({
-//           file: file,
-//           imagePreviewUrl: reader.result
-//         });
-//       }
-//       reader.readAsDataURL(file);
-//     }
-//     editName = e =>{
-//       const name = e.target.value;
-//       this.setState({
-//         name,
-//       });
-//     }
-
-//     editage = e => {
-//       const age = e.target.value;
-//       this.setState({
-//         age,
-//       });
-//     }
-
-//     editgender = e => {
-//         const gender = e.target.value;
-//         this.setState({
-//           gender,
-//         });
-//       }
-
-//       editMobile= e => {
-//         const mobile = e.target.value;
-//         this.setState({
-//           mobile,
-//         });
-//       }
-
-//       editAddress = e => {
-//         const address = e.target.value;
-//         this.setState({
-//           address,
-//         });
-//       }
-
-//       editBio = e => {
-//         const bio = e.target.value;
-//         this.setState({
-//           bio,
-//         });
-//       }
-//     handleSubmit= e =>{
-//       e.preventDefault();
-//       let activeP = this.state.active === 'edit' ? 'profile' : 'edit';
-//       this.setState({
-//         active: activeP,
-//       })
-//     }
-
-//     render() {
-//       const {imagePreviewUrl, 
-//              name, 
-//              age, 
-//              gender,
-//              mobile,
-//              address,
-//              bio,
-//              active} = this.state;
-//       return (
-//         <div>
-//         {(active === 'edit')?(
-//           <Edit onSubmit={this.handleSubmit}>
-
-//               <ImgUpload onChange={this.photoUpload} src={imagePreviewUrl}/>
-//               <Name onChange={this.editName} value={name}/>
-//               <Age onChange={this.editage} value={age}/>
-//               <Gender onChange={this.editgender} value={gender}/>
-//               <Mobile onChange={this.editMobile} value={mobile}/>
-//               <Address onChange={this.editAddress} value={address}/>
-//               <Bio onChange={this.editBio} value={bio}/>
-//             </Edit>
-//         ):(
-//             <Profile 
-//               onSubmit={this.handleSubmit} 
-//               src={imagePreviewUrl} 
-//               name={name} 
-//               age={age}
-//               gender={gender}
-//               mobile={mobile}
-//               address={address}
-//               bio={bio}
-//               />)}
-
-//         </div>
-
-//       )
-
-//     }
-//   }
-
-//  export default CardProfile;
 
 const userProfile = () => {
   return (<>
 
-    <div class="one">
+    <div className="mainsection">
 
-      <div class="cover">
-        <img scr="cover.jpg" width="1000"></img>
-        {/* <h1>wetewt</h1> */}
+      <div className='left'>
+
+        <h1>Name : Bimsara Kulasekara</h1>
+        <h1>Age : 22 Yrs</h1>
+        <h1>Gender : Male</h1>
+        <h1>Mobile : 0785619959</h1>
+        <h1>Address : No. 123/A Mirigama</h1>
+        <h1>Bio : I’m a professional developer with experience working with technologies like Native android development, Mobile cross-platform development, backend/frontend development and Firebase & Google Cloud platforms. </h1>
+
+
+
+
+      </div>
+      <div className='right'>
+        <h1>Name : Bimsara Kulasekara</h1>
+
+
       </div>
 
 
